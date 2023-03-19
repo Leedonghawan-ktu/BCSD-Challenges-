7aff5dfddceb4e814db08dc7221f8cef41b7239c
Leedonghawan-ktu <128292010+Leedonghawan-ktu@users.noreply.github.com>
2023-03-20 05:05:54 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Learning
+{
+	class Program
+	{
+		static void Main()
+		{
+			string starpicture = "";
+			int i, j;
+			for (i = 0; i < 5; ++i)
+			{
+				starpicture = "";
+				for (j = i; j < 5 - 1; ++j)
+				{
+					starpicture += "★";
+				}
+				for (j = 0; j <= i * 2; ++j)
+				{
+					starpicture += "☆";
+				}
+				for (j = i; j < 5 - 1; ++j)
+				{
+					starpicture += "★";
+				}
+				Console.WriteLine(starpicture);
+			}
+			for (i = 0; i < 4; ++i)
+			{
+				starpicture = "";
+
+				for (j = 0; j <= i; ++j)
+				{
+					starpicture += "★";
+				}
+				for (j = i * 2; j < 4 * 2 - 1; ++j)
+				{
+					starpicture += "☆";
+				}
+				for (j = 0; j <= i; ++j)
+				{
+					starpicture += "★";
+				}
+				Console.WriteLine(starpicture);
+			}
+		}
+	}
+}
